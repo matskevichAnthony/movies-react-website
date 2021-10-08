fa93b6291cc00dfa625b0be258fbc6fa855a409b
matskevichAnthony <91896307+matskevichAnthony@users.noreply.github.com>
2021-10-08 12:30:28 +0100

    Our work page is styled and connected

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
 import ContactUs from './pages/ContactUs';
 import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
 
 //router
 
@@ -21,11 +22,14 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
         </Route>
         <Route path="/contact">
           <ContactUs />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
       </Switch>
       <GlobalStyle />
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -12,21 +12,21 @@ const OurWork = () => {
             <Movie>
                 <h2>The athlete</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/the-athlete">
                     <img src={athlete} alt="athlete" />
                 </Link>
             </Movie>
             <Movie>
                 <h2>The racer</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/the-racer">
                     <img src={theracer} alt="racer" />
                 </Link>
             </Movie>
             <Movie>
                 <h2>Good times</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/good-times">
                     <img src={goodtimes} alt="goodtimes" />
                 </Link>
             </Movie>
