9b8f16bf7c7085134e7304c6924919e5e4b0d623
matskevichAnthony <91896307+matskevichAnthony@users.noreply.github.com>
2021-10-12 12:04:30 +0100

    About us section modified

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,7 +3,7 @@ import home1 from '../img/home1.png'
 //styled
 import { About, Description, Image, Hide } from '../styles';
 import { motion } from 'framer-motion';
-
+import { titleAnim, fade, photoAnim } from '../animation';
 const AboutSection = () => {
 
 
@@ -12,23 +12,23 @@ const AboutSection = () => {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >true</motion.h2>
+                        <motion.h2 variants={titleAnim}>true</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or
+                <motion.p variants={fade}>Contact us for any photography or
                     videography ideas that you have!
                     We have professionals with amazing skills.
-                </p>
-                <button>Contact Us</button>
+                </motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="" />
+                <motion.img variants={photoAnim} src={home1} alt="" />
             </Image>
         </About >
     )
